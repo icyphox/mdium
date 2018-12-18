55af93dbf1f6be47ca7ede2ad12acb1dd4ef8dd6
Anirudh <icyph0x@pm.me>
2018-12-18 09:04:49 +0530

    Handle the usual errors
    Signed-off-by: Anirudh <icyph0x@pm.me>

--- mdium/mdium.py
+++ mdium/mdium.py
@@ -2,11 +2,17 @@ import requests
 import os
 import json
 import frontmatter
+import sys
 
 def init(token):
     headers = {'Authorization': 'Bearer {}'.format(token)}
     r = requests.get('https://api.medium.com/v1/me', headers=headers)
-    user_data = r.json()['data']
+    try:
+        user_data = r.json()['data']
+    except KeyError:
+        print('error: invalid integration token')
+        sys.exit(1)
+
     conf = {
             'token': token,
             'id': user_data['id']
@@ -18,24 +24,35 @@ def init(token):
 
 
 def read_conf():
-    with open(os.path.expanduser('~/.mdium'), 'r') as f:
-        conf = json.load(f)
-        return conf
+    try:
+        with open(os.path.expanduser('~/.mdium'), 'r') as f:
+            conf = json.load(f)
+            return conf
+    except FileNotFoundError:
+        print('error: the `~/.mdium` file is missing. Have you run `mdium init <token>`?')
+        sys.exit(1)
 
 
 def publish(mdfile):
     conf = read_conf()
-    with open(mdfile, 'r') as f:
-        content = frontmatter.load(f)
-
-    headers = {'Authorization': 'Bearer {}'.format(conf['token'])}
-    body = {
-            'title': content['title'],
-            'contentFormat': 'markdown',
-            'content': content.content,
-            'tags': content['tags'],
-            'publishStatus': content['status']
-           }
+    try:
+        with open(mdfile, 'r') as f:
+            content = frontmatter.load(f)
+    except FileNotFoundError:
+        print('error: the file specified was not found')
+        sys.exit(1)
+    try: 
+        headers = {'Authorization': 'Bearer {}'.format(conf['token'])}
+        body = {
+                'title': content['title'],
+                'contentFormat': 'markdown',
+                'content': content.content,
+                'tags': content['tags'],
+                'publishStatus': content['status']
+               }
+    except KeyError:
+        print('error: check the frontmatter in your markdown doc')
+        sys.exit(1)
     r = requests.post(
             'https://api.medium.com/v1/users/{}/posts'.format(conf['id']), 
             headers=headers,
